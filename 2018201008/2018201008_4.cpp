25c711ac7f88ad96ffbfad7a3eca387e0bd6d0e9
Tarun <tarun.mohandas@gmail.com>
2018-09-03 10:38:07 +0530

    vector implementation done

--- 2018201008/2018201008_4.cpp
+++ 2018201008/2018201008_4.cpp
@@ -14,10 +14,9 @@ class vector
 	
 	vector()
 	{
-		array = (int *)calloc(1,sizeof(int));
 		start_index = 0;
 		end_index = 0;
-		max_size = 1;
+		max_size = 0;
 	}
 	
 	vector(int n,int x)
@@ -35,12 +34,12 @@ class vector
 	
 	void push_back(int x)
 	{
-		if(end_index<max_size)
+		if(max_size<=0)
 		{
-			array[end_index] = x;
-			end_index++;
+			array = (int *)calloc(1,sizeof(int));
+			max_size = 1;
 		}
-		else
+		else if(end_index>=max_size)
 		{
 			max_size = max_size*2;
 			int * new_array = (int *)calloc(max_size,sizeof(int));
@@ -49,9 +48,9 @@ class vector
 				new_array[i] = array[i];
 			}
 			array = new_array;
-			array[end_index] = x;
-			end_index++;
 		}
+		array[end_index] = x;
+		end_index++;
 	}
 	
 	void pop_back()
@@ -60,7 +59,10 @@ class vector
 		if(end_index<=(max_size/2))
 		{
 			max_size = max_size/2;
-			max_size = (max_size<=0)?1:max_size;
+			if(max_size<=0)
+			{
+				return;
+			}
 			int * new_array = (int *)calloc(max_size,sizeof(int));
 			for(int i=start_index; i<end_index; i++)
 			{
@@ -70,6 +72,58 @@ class vector
 		}
 	}
 	
+	void insert(int index,int x)
+	{
+		if(index<start_index || index>=end_index)
+		{
+			cout<<"\nArray index out of bounds.\n";
+			exit(0);
+		}
+		if(end_index>=max_size)
+		{
+			max_size = max_size*2;
+			int * new_array = (int *)calloc(max_size,sizeof(int));
+			for(int i=start_index; i<end_index; i++)
+			{
+				new_array[i] = array[i];
+			}
+			array = new_array;
+		}
+		for(int i=(end_index-1); i>=index; i--)
+		{
+			array[i+1] = array[i];
+		}
+		array[index] = x;
+		end_index++;
+	}
+	
+	void erase(int index)
+	{
+		if(index<start_index || index>=end_index)
+		{
+			cout<<"\nArray index out of bounds.\n";
+			exit(0);
+		}
+		for(int i=(index+1); i<end_index; i++)
+		{
+			array[i-1] = array[i];
+		}
+		end_index--;
+		if(end_index<=(max_size/2))
+		{
+			max_size = max_size/2;
+			if(max_size<=0)
+			{
+				return;
+			}
+			int * new_array = (int *)calloc(max_size,sizeof(int));
+			for(int i=start_index; i<end_index; i++)
+			{
+				new_array[i] = array[i];
+			}
+			array = new_array;
+		}
+	}
 	
 	int operator[](int index)
 	{
@@ -85,6 +139,27 @@ class vector
 	{
 		return end_index;
 	}
+	
+	int front()
+	{
+		if(end_index==0)
+		{
+			cout<<"\nArray index out of bounds\n";
+			exit(0);
+		}
+		return array[start_index];
+	}
+	
+	int back()
+	{
+		if(end_index==0)
+		{
+			cout<<"\nArray index out of bounds\n";
+			exit(0);
+		}
+		return array[end_index-1];
+	}
+	
 };
 
 
@@ -92,15 +167,21 @@ class vector
 int main()
 {
 	vector vec;
-	for(int i=1; i<=32; i++)
+	for(int i=1; i<=100; i++)
 	{
 		vec.push_back(i);
 		cout<<endl<<"Pushed "<<i<<" size="<<vec.size()<<" max size = "<<vec.max_size<<endl;
 	}
-	for(int i=32; i>=1; i--)
+	for(int i=1; i<=30; i++)
 	{
-		vec.pop_back();
-		cout<<endl<<"Popped "<<i<<" size="<<vec.size()<<" max size = "<<vec.max_size<<endl;
+		vec.erase(6);
+		cout<<endl<<"Erased "<<i<<" size="<<vec.size()<<" max size = "<<vec.max_size<<endl;
 	}
+	cout<<endl;
+	for(int i=0; i<vec.size(); i++)
+	{
+		cout<<vec[i]<<"  ";
+	}
+	cout<<endl;
 	return 0;
 }
