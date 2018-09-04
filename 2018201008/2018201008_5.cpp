ff487cf6d1abd8665a100b41c66b730134de9b5b
Tarun <tarun.mohandas@gmail.com>
2018-09-04 18:52:25 +0530

    median done

---
+++ 2018201008/2018201008_5.cpp
@@ -0,0 +1,444 @@
+#include<iostream>
+#include<queue>
+#include<stdio.h>
+
+using namespace std;
+
+class min_priority_queue
+{
+	public:
+	
+	float * pri_queue;
+	int size;
+	int max_size;
+	
+	min_priority_queue(int n)
+	{
+		pri_queue = (float *)malloc(n*sizeof(float));
+		size = 0;
+		max_size = n;
+	}
+	
+	int get_heap_size()
+	{
+		return size;
+	}
+	
+	float get_min_element()
+	{
+		if(size==0)
+		{
+			cout<<"\n\nHeap is empty\n\n";
+			exit(1);
+		}
+		return pri_queue[0];
+	}
+	
+	int get_parent(int index)
+	{
+		if(index==0)
+		{
+			return -1;
+		}
+		if(index%2==0)
+		{
+			return ((index/2)-1);
+		}
+		if(index%2!=0)
+		{
+			return (index/2);
+		}
+	}
+	
+	int get_left_child(int index)
+	{
+		int lc = (index*2) + 1;
+		if(lc>=size)
+		{
+			return -1;
+		}
+		return lc;
+	}
+	
+	int get_right_child(int index)
+	{
+		int rc = (index*2)+2;
+		if(rc>=size)
+		{
+			return -1;
+		}
+		return rc;
+	}
+	
+	void insert(float x)
+	{
+		if(size>=max_size)
+		{
+			return;
+		}
+		pri_queue[size] = x;
+		int parent = get_parent(size);
+		int curr_index = size;
+		while(parent>-1)
+		{
+			if(pri_queue[parent]>pri_queue[curr_index])
+			{
+				float temp = pri_queue[parent];
+				pri_queue[parent] = pri_queue[curr_index];
+				pri_queue[curr_index] = temp;
+				curr_index = parent;
+				parent = get_parent(curr_index);
+			}
+			else
+			{
+				break;
+			}
+		}
+		size++;
+	}
+	
+	float extract_min()
+	{
+		if(size==0)
+		{
+				cout<<"\n\nHeap is empty\n\n";
+				exit(1);
+		}
+		if(size==1)
+		{
+			float x = pri_queue[0];
+			size--;
+			return x;
+		}
+		float min = pri_queue[0];
+		pri_queue[0] = pri_queue[size-1];
+		pri_queue[size-1] = min;
+		size--;
+		int c_i = 0;
+		int l_i = get_left_child(c_i);
+		int r_i = get_right_child(c_i);
+		while(l_i>=0 && l_i<size)
+		{
+			int min_i = c_i;
+			if(pri_queue[l_i] < pri_queue[c_i])
+			{
+				min_i = l_i;
+			}
+			if((r_i>=0 && r_i < size) && pri_queue[r_i] < pri_queue[min_i])
+			{
+				min_i = r_i;
+			}
+			if(c_i == min_i)
+			{
+				break;
+			}
+			else
+			{
+				float temp = pri_queue[c_i];
+				pri_queue[c_i] = pri_queue[min_i];
+				pri_queue[min_i] = temp;
+			}
+			c_i = min_i;
+			l_i = get_left_child(c_i);
+			r_i = get_right_child(c_i);
+		}
+		return min;
+		
+	}
+	
+	void print_tree()
+	{
+		if(size==0)
+		{
+			cout<<"\n\nHeap is empty\n\n";
+			return;
+		}
+		cout<<"\n\n";
+		queue<int> iq;
+		iq.push(0);
+		while(!iq.empty())
+		{
+			int i = iq.front();
+			iq.pop();
+			cout<<pri_queue[i]<<"   ";
+			int lc = get_left_child(i);
+			int rc = get_right_child(i);
+			if(lc<size && lc!=-1)
+			{
+				iq.push(lc);
+			}
+			if(rc<size && rc!=-1)
+			{
+				iq.push(rc);
+			}
+		}
+		cout<<"\n\n";
+	}
+	
+	
+};
+
+class max_priority_queue
+{
+	public:
+	
+	float * pri_queue;
+	int size;
+	int max_size;
+	
+	max_priority_queue(int n)
+	{
+		pri_queue = (float *)malloc(n*sizeof(float));
+		size = 0;
+		max_size = n;
+	}
+	
+	int get_heap_size()
+	{
+		return size;
+	}
+	
+	float get_max_element()
+	{
+		if(size==0)
+		{
+			cout<<"\n\nHeap is empty\n\n";
+			exit(1);
+		}
+		return pri_queue[0];
+	}
+	
+	int get_parent(int index)
+	{
+		if(index==0)
+		{
+			return -1;
+		}
+		if(index%2==0)
+		{
+			return ((index/2)-1);
+		}
+		if(index%2!=0)
+		{
+			return (index/2);
+		}
+	}
+	
+	int get_left_child(int index)
+	{
+		int lc = (index*2) + 1;
+		if(lc>=size)
+		{
+			return -1;
+		}
+		return lc;
+	}
+	
+	int get_right_child(int index)
+	{
+		int rc = (index*2)+2;
+		if(rc>=size)
+		{
+			return -1;
+		}
+		return rc;
+	}
+	
+	void insert(float x)
+	{
+		if(size>=max_size)
+		{
+			return;
+		}
+		pri_queue[size] = x;
+		int parent = get_parent(size);
+		int curr_index = size;
+		while(parent>-1)
+		{
+			if(pri_queue[parent]<pri_queue[curr_index])
+			{
+				float temp = pri_queue[parent];
+				pri_queue[parent] = pri_queue[curr_index];
+				pri_queue[curr_index] = temp;
+				curr_index = parent;
+				parent = get_parent(curr_index);
+			}
+			else
+			{
+				break;
+			}
+		}
+		size++;
+	}
+	
+	float extract_max()
+	{
+		if(size==0)
+		{
+				cout<<"\n\nHeap is empty\n\n";
+				exit(1);
+		}
+		if(size==1)
+		{
+			float x = pri_queue[0];
+			size--;
+			return x;
+		}
+		float max = pri_queue[0];
+		pri_queue[0] = pri_queue[size-1];
+		pri_queue[size-1] = max;
+		size--;
+		int c_i = 0;
+		int l_i = get_left_child(c_i);
+		int r_i = get_right_child(c_i);
+		while(l_i>=0 && l_i<size)
+		{
+			int max_i = c_i;
+			if(pri_queue[l_i] > pri_queue[c_i])
+			{
+				max_i = l_i;
+			}
+			if((r_i>=0 && r_i < size) && pri_queue[r_i] > pri_queue[max_i])
+			{
+				max_i = r_i;
+			}
+			if(c_i == max_i)
+			{
+				break;
+			}
+			else
+			{
+				float temp = pri_queue[c_i];
+				pri_queue[c_i] = pri_queue[max_i];
+				pri_queue[max_i] = temp;
+			}
+			c_i = max_i;
+			l_i = get_left_child(c_i);
+			r_i = get_right_child(c_i);
+		}
+		return max;
+		
+	}
+	
+	void print_tree()
+	{
+		if(size==0)
+		{
+			cout<<"\n\nHeap is empty\n\n";
+			return;
+		}
+		cout<<"\n\n";
+		queue<int> iq;
+		iq.push(0);
+		while(!iq.empty())
+		{
+			int i = iq.front();
+			iq.pop();
+			cout<<pri_queue[i]<<"   ";
+			int lc = get_left_child(i);
+			int rc = get_right_child(i);
+			if(lc<size && lc!=-1)
+			{
+				iq.push(lc);
+			}
+			if(rc<size && rc!=-1)
+			{
+				iq.push(rc);
+			}
+		}
+		cout<<"\n\n";
+	}
+	
+	
+};
+
+int main()
+{
+	int N;
+	scanf("%d",&N);
+	int W = ((N+4)/2);
+	max_priority_queue MAX = max_priority_queue(W);
+	min_priority_queue MIN = min_priority_queue(W);
+	for(int i=0; i<N; i++)
+	{
+		float e;
+		scanf("%f",&e);
+		if(MAX.get_heap_size()==0)
+		{
+			MAX.insert(e);
+		}
+		else
+		{
+			float max_max = MAX.get_max_element();
+			//int min_min = MIN.get_min_element();
+			if(e <= max_max)
+			{
+				if(MAX.get_heap_size() > MIN.get_heap_size())
+				{
+					max_max = MAX.extract_max();
+					MIN.insert(max_max);
+					MAX.insert(e);
+				}
+				else
+				{
+					MAX.insert(e);
+				}
+			}
+			else if(e>max_max && MIN.get_heap_size()==0)
+			{
+				MIN.insert(e);
+			}
+			else
+			{
+				float min_min = MIN.get_min_element();
+				if(e>max_max && e<=min_min)
+				{
+					if(MAX.get_heap_size() == MIN.get_heap_size())
+					{
+						MAX.insert(e);
+					}
+					else if(MAX.get_heap_size()>MIN.get_heap_size())
+					{
+						MIN.insert(e);
+					}
+					else
+					{
+						MAX.insert(e);
+					}
+				}
+				else if(e > max_max && e > min_min)
+				{
+					if(MIN.get_heap_size() < MAX.get_heap_size())
+					{
+						MIN.insert(e);
+					}
+					else
+					{
+						min_min = MIN.extract_min();
+						MIN.insert(e);
+						MAX.insert(min_min);
+					}
+				}
+			}
+		}
+		if((MIN.get_heap_size()==0) || (MIN.get_heap_size() < MAX.get_heap_size()))
+		{
+			printf("%0.1f",(double)MAX.get_max_element());
+		}
+		else if((MAX.get_heap_size()) == (MIN.get_heap_size()))
+		{
+			float a = MAX.get_max_element();
+			float b = MIN.get_min_element();
+			printf("%0.1f",(double)((a+b)/2));
+		}
+		else
+		{
+			printf("%0.1f",(double)MIN.get_min_element());
+		}
+		if(i!=(N-1))
+		{
+				cout<<"\n";
+		}
+	}
+	return 0;
+}
